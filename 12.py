12c63c7499f01da19bdc7589599f678079e9bea4
FemkeNijman <55234333+FemkeNijman@users.noreply.github.com>
2019-11-05 16:33:27 +0100

    Update 12.py

--- 12.py
+++ 12.py
@@ -1,5 +1,5 @@
 def product(getal):
-    """Vermenigvuldigd het getal met zichzelf en elk getal wat daarvoor komt.
+    """vermenigvuldigd het getal met zichzelf en elk getal wat daarvoor komt.
     
     input: 
     getal - int
